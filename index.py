44ba642c9a9c77c925b3199f83f681016a5704fa
nathandantasn <65520727+nathandantasn@users.noreply.github.com>
2021-06-03 16:32:41 -0300

    Delete index.py

--- index.py
+++
@@ -1,9 +0,0 @@
-# Faça um programa que receba a idade do usuário e diga se ele é maior ou menor de idade. 
-
-print("Olá! Digite sua idade!")
-
-idade = int(input("Idade: "))
-if idade >= 18:
-  print("Você é maior de idade")
-else:
-  print("Você é menor de idade")
